a49758f6ef14b7038c3cd2ef2c030ad49bb0f974
Jerome <dreammedia365@yahoo.com>
2020-08-03 12:37:17 -0700

    Updates

---
+++ precache-manifest.9df5b4936056da4b1e8ee7a8cefc56b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d213cb7c59cd9bfaa7eff8fd4b85cfd",
+    "url": "/Samsung-Application/index.html"
+  },
+  {
+    "revision": "c2d2f57d358cabe1a019",
+    "url": "/Samsung-Application/static/css/main.58913191.chunk.css"
+  },
+  {
+    "revision": "06df05d84338335c7b81",
+    "url": "/Samsung-Application/static/js/2.b3847bfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Samsung-Application/static/js/2.b3847bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d2f57d358cabe1a019",
+    "url": "/Samsung-Application/static/js/main.229af283.chunk.js"
+  },
+  {
+    "revision": "333af47ca01b5b71e602",
+    "url": "/Samsung-Application/static/js/runtime-main.982508e8.js"
+  }
+]);
